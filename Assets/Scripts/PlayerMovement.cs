a0314d84a17aa0e3fc6e98c48b290ccc17de33e9
İbrahim Gümüşdal <ibooo2977@gmail.com>
2024-11-03 15:20:41 +0300

    Trying 03.11.24

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     float speed = 5f;
+    float dashSpeed = 20f;
+    float dashTime = 0.2f;
+    float doubleTabTime = 0.2f;
+
+    private float lastTa,pTimeA, lastTapTimeD;
+    private bool isDash;
+    private Vector2 dashDirection;
+    private float dashTimer;
     void Start()
     {
         
@@ -14,7 +23,14 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         float xMovement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(speed * xMovement * Time.deltaTime,0,0);
-        
+        transform.position += new Vector3(speed * xMovement * Time.deltaTime, 0, 0);
+
+        if (isDash)
+        {
+
+        }
+
     }
+
+    
 }
