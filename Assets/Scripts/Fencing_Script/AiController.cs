5c3f95040612814a2d433d6769719f118c175f72
İbrahim Gümüşdal <ibooo2977@gmail.com>
2024-12-01 20:19:41 +0300

    01.12.2024
    Updating and Developing AI
    Decreasing Code Files and Lines
    Fixing Bugs
    Adding Walls

---
+++ Assets/Scripts/Fencing_Script/AiController.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AiController : MonoBehaviour
+{
+    public string aiStatusCombat;
+    public string aiStatusMoving;
+
+    //UI
+    public Text aiStunnedUI;
+    public Text aiCombatStatusUI;
+    public Text aiStaminaUI;
+    public Text aiMovementStatusUI;
+    public Text aiIsHit;
+
+    public Transform player;
+    public LayerMask oppositePlayerAI;
+    public Transform attackPointTopAI, attackPointBottomAI;
+    public float attackRangeAI = 0.5f;
+    public float aiCloseCombatRange = 2f;
+
+    private float speedAI = 3f;
+    private float thinkTime = 1.5f;
+    private bool canDoSomething = true;
+
+    private float staminaAI, maxStamina = 100f;
+    private float staminaRegenSpeed = 5f;
+
+
+    void Start()
+    {
+        staminaAI = maxStamina;
+
+        aiCombatStatusUI.text = "";
+        aiStunnedUI.text = "start and no stun";
+        aiStaminaUI.text = "";
+        aiMovementStatusUI.text = " ";
+        aiIsHit.text = "is hit: no";
+    }
+
+    void Update()
+    {
+        //float distanceToPlayer = Vector3.Distance(player.position, transform.position);
+        //Debug.Log(distanceToPlayer);
+        AiUpdatingUI();
+        RegenerateStamina();
+        if (canDoSomething)
+        {
+            StartCoroutine(ThinkAndDoingSomething());
+        }
+    }
+
+    IEnumerator ThinkAndDoingSomething()
+    {
+        canDoSomething = false;
+
+        int action = Random.Range(0, 5); // 0: idle, 1: move, 2: topAttack, 3: bottomAttack
+
+        if (staminaAI <= 0)
+        {
+            aiStun(2f);
+        }
+        else if (staminaAI <= 20)
+        {
+            action = 0;
+        }
+
+        switch (action)
+        {
+            case 0:
+
+                break;
+
+            case 1:
+                StartCoroutine(AIMovement('r'));
+                break;
+
+            case 2:
+                StartCoroutine(AIMovement('l'));
+                break;
+
+            case 3:
+                if (staminaAI >= 20)
+                {
+                    TopAttack();
+                }
+                break;
+
+            case 4:
+                if (staminaAI >= 20)
+                {
+                    BottomAttack();
+
+                }
+                break;
+        }
+
+        yield return new WaitForSeconds(thinkTime);
+        aiStatusCombat = "thinking";
+        aiStatusMoving = "thinking";
+
+        canDoSomething = true;
+    }
+
+    IEnumerator AIMovement(char whichDirection)
+    {
+        float moveDuration = Random.Range(0f, 3f);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < moveDuration)
+        {
+            float distanceToPlayer = Vector3.Distance(player.position, transform.position);
+            if (whichDirection == 'l'&& distanceToPlayer>1.5f)
+            {
+                Vector3 direction = (player.position - transform.position).normalized;
+                transform.position += direction * speedAI * Time.deltaTime;
+
+                aiStatusMoving = "moving_left";
+            }
+            else if (whichDirection == 'r' && !IsWallBehind())
+            {
+
+                Vector3 direction = (transform.position - player.position).normalized;
+                transform.position += direction * speedAI * Time.deltaTime;
+
+                aiStatusMoving = "moving_right";
+            }
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(moveDuration);
+    }
+    bool IsWallBehind()
+    {
+        Vector2 origin = transform.position;
+        Vector2 direction = Vector2.right;
+        RaycastHit2D isWall = Physics2D.Raycast(origin, direction, 1f, LayerMask.GetMask("wall"));
+        
+        return isWall;
+    }
+
+    void TopAttack()
+    {
+        aiStatusCombat = "top attack wait";
+        takingStance();
+        if (staminaAI >= 20)
+        {
+            staminaAI -= 20f;
+            aiStatusCombat = "top attack";
+            Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPointTopAI.position, attackRangeAI, oppositePlayerAI);
+            foreach (Collider2D Player_1 in hitPlayer)
+            {                
+                StartCoroutine(HittingWaitForASeconds());
+
+                Debug.Log("Enemy bot hit to player!");
+            }
+        }
+    }
+
+    void BottomAttack()
+    {
+        aiStatusCombat = "bot attack wait";
+        takingStance();
+        if (staminaAI >= 20)
+        {
+
+            staminaAI -= 20f;
+            aiStatusCombat = "bot attack";
+            Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPointBottomAI.position, attackRangeAI, oppositePlayerAI);
+            foreach (Collider2D Player_1 in hitPlayer)
+            {
+                
+                StartCoroutine(HittingWaitForASeconds());
+                
+                Debug.Log("Enemy bot hit to player!");
+            }
+        }
+    }
+
+    void takingStance()
+    {
+
+        StartCoroutine(waitingTakingStance());
+    }
+    IEnumerator waitingTakingStance()
+    {
+
+        float time = Random.Range(0.5f, 3f);
+        yield return new WaitForSeconds(time);
+    }
+    void RegenerateStamina()
+    {
+        if (staminaAI < maxStamina)
+        {
+            staminaAI += Time.deltaTime * staminaRegenSpeed;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPointTopAI == null || attackPointBottomAI == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPointTopAI.position, attackRangeAI);
+        Gizmos.DrawWireSphere(attackPointBottomAI.position, attackRangeAI);
+    }
+
+    public void aiStun(float duration)
+    {
+        StartCoroutine(AiStunCoroutine(duration));
+    }
+    private IEnumerator AiStunCoroutine(float stunDuration)
+    {
+        canDoSomething = false;
+        //stun animation
+
+        aiStaminaUI.text = "stunned";
+        aiCombatStatusUI.text = "stunned";
+        aiMovementStatusUI.text = "stunned";
+        yield return new WaitForSeconds(stunDuration);
+        aiStunnedUI.text = "no stunned";
+        canDoSomething = true;
+    }
+
+    public void AiUpdatingUI()
+    {
+        aiStaminaUI.text = "Stamina: " + staminaAI.ToString("F0");
+        aiCombatStatusUI.text = aiStatusCombat;
+        aiMovementStatusUI.text = aiStatusMoving;
+    }
+
+    IEnumerator HittingWaitForASeconds()
+    {
+        aiIsHit.text = "hit: yes";
+        yield return new WaitForSeconds(1f);
+        aiIsHit.text = "hit: no";
+    }
+
+
+
+    
+}
---
+++ Assets/Scripts/Fencing_Script/Player_Movement_Combat.cs
@@ -0,0 +1,351 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
+using UnityEditor.Build.Content;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Movement_Combat : MonoBehaviour
+{
+    bool canAttackorDefence = true;
+
+    //playerstatus
+    public string playerStatusCombat;
+    public string playerStatusMoving;
+
+    //UI
+    public Text stunnedUI;
+    public Text combatStatusUI;
+    public Text staminaUI;
+    public Text movementStatusUI;
+    public Text isHit;
+
+
+    //opposite player layer
+    public LayerMask oppositePlayer;
+
+    //combat
+    public float attackRange = 0.5f;
+    public Transform attackPointTop, attackPointBottom;
+
+    //stamina
+    protected internal float 
+        stamina, 
+        maxStamina = 100f, 
+        minStamina = 0f;
+    private float staminaRegenSpeed = 10f;
+
+    //movement statistics
+    float speed = 5f;
+    float dashSpeed = 20f;
+    float dashTime = 0.2f;
+    float doubleTabTime = 0.2f;
+
+    //about dash
+    private float lastTapTimeA, lastTapTimeD;
+    private bool isDash;
+    private Vector2 dashDirection;
+    private float dashTimer;
+
+    protected internal bool canMove = true;
+
+    void Start()
+    {
+        staminaUI.text = "";
+        stunnedUI.text = "start and no stun";
+        combatStatusUI.text = "";
+        movementStatusUI.text = " ";
+        isHit.text = "hit: no";
+
+        stamina = maxStamina;
+    }
+
+
+    void Update()
+    {
+        updatingUI();
+        staminaRegeneration(staminaRegenSpeed);//stamina regen
+
+        //movement
+
+        if (canMove)
+        {
+            if (isDash)
+            {
+
+                transform.Translate(dashSpeed * dashDirection * Time.deltaTime);
+
+                dashTimer -= Time.deltaTime;
+                if (dashTimer < 0)
+                {
+                    isDash = false;
+                }
+            }
+            else
+            {
+                walking();
+                dashInput(stamina);
+            }
+            //Debug.Log(playerStatus);
+        }
+
+        if (stamina == 0f)
+        {
+            stunned(1f);
+        }
+
+        if (canAttackorDefence)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    if (stamina >= 20)
+                    {
+                        topAttacking();
+
+                    }
+                    else
+                    {
+                        stunned(3f);
+                    }
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    if (stamina >= 20)
+                    {
+                        bottomAttacking();
+
+                    }
+                    else
+                    {
+                        stunned(3f);
+                    }
+                }
+                else
+                {
+                    attackIdlePosition();
+                }
+            }
+
+            else if (Input.GetMouseButton(1))
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    topDefence();
+                }
+                else if (Input.GetKey(KeyCode.S))
+                {
+                    bottomDefence();
+                }
+                else
+                {
+                    defendIdlePosition();
+                }
+            }
+
+            else
+            {
+                idlePosition();
+            }
+        }
+
+    }
+    //movement
+    void walking()
+    {
+
+        float xMovement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(speed * xMovement * Time.deltaTime, 0, 0);
+        if (Input.GetAxis("Horizontal") >= 0.10)
+        {
+            playerStatusMoving = "walkingRight";
+        }
+        else if (Input.GetAxis("Horizontal") <= -0.10)
+        {
+            playerStatusMoving = "walkingLeft";
+        }
+        else
+        {
+            playerStatusMoving = "idle";
+        }
+
+    }
+    //dash
+    void dashInput(float stamina)
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            if (Time.time - lastTapTimeA < doubleTabTime)
+            {
+                if (stamina >= 15)
+                {
+                    StartDash(Vector2.left);
+                    playerStatusMoving = "dashingLeft";
+                    staminaCost(15f);
+                }
+                else if (stamina < 15)
+                {
+                    stunned(2f);
+                }
+
+            }
+            lastTapTimeA = Time.time;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+            if (Time.time - lastTapTimeD < doubleTabTime)
+            {
+                if (stamina >= 15)
+                {
+                    StartDash(Vector2.right);
+                    playerStatusMoving = "dashingRight";
+                    staminaCost(15f);
+                }
+                else if (stamina < 15)
+                {
+                    stunned(2f);
+                }
+            }
+            lastTapTimeD = Time.time;
+        }
+    }
+
+    private void StartDash(Vector2 direction)
+    {
+        isDash = true;
+        dashDirection = direction;
+        dashTimer = dashTime;
+    }
+
+    //attack
+    void topAttacking()
+    {
+        //top attack animation
+        staminaCost(20f);
+        playerStatusCombat = "top_attack";
+
+        Collider2D[] hitEnemy = Physics2D.OverlapCircleAll(attackPointTop.position, attackRange, oppositePlayer);
+        foreach (Collider2D Player_2 in hitEnemy)
+        {
+            Debug.Log("Hit to player 2");
+            
+            StartCoroutine(HittingWaitForASeconds());
+            
+        }
+    }
+
+    void bottomAttacking()
+    {
+        //top attack animation
+        staminaCost(20f);
+        playerStatusCombat = "bot_attack";
+
+        Collider2D[] hitEnemy = Physics2D.OverlapCircleAll(attackPointBottom.position, attackRange, oppositePlayer);
+        foreach (Collider2D Player_2 in hitEnemy)
+        {
+            Debug.Log("Hit to player 2");
+            
+            StartCoroutine(HittingWaitForASeconds());
+            
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPointTop == null || attackPointBottom == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPointTop.position, attackRange);
+        Gizmos.DrawWireSphere(attackPointBottom.position, attackRange);
+    }
+    //defence
+    void topDefence()
+    {
+        //top defence  animation
+        playerStatusCombat = "top_defence";
+    }
+
+    void bottomDefence()
+    {
+        //bot defence  animation
+        playerStatusCombat = "bottom_defence";
+    }
+    //idle 
+    void attackIdlePosition()
+    {
+        //attack idle animation
+        playerStatusCombat = "attack idle";
+    }
+    void defendIdlePosition()
+    {
+        //defend idle animation
+        playerStatusCombat = "defend idle";
+    }
+    void idlePosition()
+    {
+        //idle position animation
+        playerStatusCombat = "idle";
+    }
+    //stun
+    public void stunned(float duration)
+    {
+        StartCoroutine(StunCoroutine(duration));
+    }
+
+    private IEnumerator StunCoroutine(float stunDuration)
+    {
+        canMove = false;
+        canAttackorDefence = false;
+
+        //stun animation
+        stunnedUI.text = "stunned";
+        playerStatusCombat = "stunned";
+        playerStatusMoving = "stunned";
+        yield return new WaitForSeconds(stunDuration);
+        stunnedUI.text = "no stunned";
+        canMove = true;
+        canAttackorDefence = true;
+    }
+    //refreshing UI
+    public void updatingUI()
+    {
+        staminaUI.text = "Stamina: " + stamina.ToString("F0");
+        combatStatusUI.text = playerStatusCombat;
+        movementStatusUI.text = playerStatusMoving;
+    }
+    //stamina
+    void staminaRegeneration(float regenSpeed)
+    {
+
+        if (stamina >= 0 && stamina < 100)
+        {
+            stamina += Time.deltaTime * regenSpeed;
+        }
+    }
+    public void staminaCost(float cost)
+    {
+        stamina -= cost;
+    }
+
+    IEnumerator WaitForSeconds(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+    }
+    
+    IEnumerator HittingWaitForASeconds()
+    {
+        isHit.text = "hit: yes";
+        yield return new WaitForSeconds(1f);
+        isHit.text = "hit: no";
+
+    }
+}
+
