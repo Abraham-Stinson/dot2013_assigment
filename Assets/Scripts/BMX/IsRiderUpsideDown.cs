ef9b36f4c66b57d9f8cc2058be7a015f88b2b8f6
İbrahim Gümüşdal <ibooo2977@gmail.com>
2024-12-10 19:51:31 +0300

    Updating BMX Again

--- Assets/Scripts/BMX/IsRiderUpsideDown.cs
+++ Assets/Scripts/BMX/IsRiderUpsideDown.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class IsRiderUpsideDown : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    public static IsRiderUpsideDown isRiderUpsideDownScript;
+    public float timer = 0;
+    public bool isWorkTimer = false;
+
+    void Start()
     {
-        if (collision.gameObject.CompareTag("Ground") && !RideBMX.instance.isGround)
+        if (isRiderUpsideDownScript == null)
         {
-            Debug.Log("Kafayı vurdu");
-            RideBMX.instance.isGround = true;
-            RideBMX.instance.upsidedPositions = RideBMX.instance.transform.position;
+            isRiderUpsideDownScript = this;
         }
     }
+
+    private void Update()
+    {
+        if (isWorkTimer)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= 1f)
+            {
+                
+                RideBMX.rideBMXScript.isGround = true;
+            }
+        }
+        else
+        {
+            timer = 0f;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) {
+            RideBMX.rideBMXScript.upsidedPositions = RideBMX.rideBMXScript.transform.position;
+            RideBMX.rideBMXScript.canInput = false;
+            RideBMX.rideBMXScript.moveInputX = 0;
+            RideBMX.rideBMXScript.moveInputY = 0;
+            isWorkTimer = true;
+        }
+    } 
 }
