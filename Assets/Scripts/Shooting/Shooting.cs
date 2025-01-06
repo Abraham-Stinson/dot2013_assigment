ea8df87fd8fe2f84a97f6d77731686392ddb90ec
Ömer Faruk Tüten <omertuten07@gmail.com>
2025-01-07 01:45:43 +0300

    shooting update
    %90 of game done

--- Assets/Scripts/Shooting/Shooting.cs
+++ Assets/Scripts/Shooting/Shooting.cs
@@ -7,16 +7,17 @@ using UnityEngine.UI;
 
 public class Shooting : MonoBehaviour
 {
-    public GameObject pointTextObj,shootAreaObj;
+    public GameObject pointTextObj,shootAreaObj,gunFireSound,timerTextObj;
     public Slider shootingSlider;
     int sliderValue = 0,points,randomSide;
+    float timer = 60;
     int sliderRandomValue;
     public int sliderShootRandomValue;
     public GameObject polarBear,handGun,leftSide,rightSide,middleSide;
     SpriteRenderer polarBearSpriteRenderer;
     bool isShootedBear = false,isShooted=false;
     bool a = true, b = false, c = false, d = false, e = false, f = false, g = false;
-    Text pointText;
+    Text pointText,timerText;
     void Start()
     {
         sliderValue = ((int)shootingSlider.value);
@@ -24,6 +25,7 @@ public class Shooting : MonoBehaviour
         sliderShootRandomValue= Random.Range(0, 101);
         polarBearSpriteRenderer = polarBear.GetComponent<SpriteRenderer>();
         pointText= pointTextObj.GetComponent<Text>();
+        timerText=timerTextObj.GetComponent<Text>();
         PolarBearSpawn();
         PolarBearWalk();
     }
@@ -33,10 +35,12 @@ public class Shooting : MonoBehaviour
         if (randomSide == 0)
         {
             polarBear.transform.position = leftSide.transform.position;
+            polarBear.transform.localScale = new Vector3(-0.37775f, 0.37775f, 0.37775f);
         }
         else if (randomSide==1)
         {
             polarBear.transform.position = leftSide.transform.position;
+            polarBear.transform.localScale = new Vector3(0.37775f, 0.37775f, 0.37775f);
         }
     }
     void PolarBearLast()
@@ -51,7 +55,7 @@ public class Shooting : MonoBehaviour
             a = false; b = false; c = false; d = false; e = false; f = false;
             g = false;
             isShootedBear = false;
-            Invoke("PolarBearLast", 0.2f);
+            Invoke("PolarBearLast", 0.1f);
         }
         if (randomSide == 1 && g == true)
         {
@@ -59,20 +63,20 @@ public class Shooting : MonoBehaviour
             a = false; b = false; c = false; d = false; e = false; f = false;
             g = false;
             isShootedBear = false;
-            Invoke("PolarBearLast", 0.2f);
+            Invoke("PolarBearLast", 0.1f);
         }
         //f
         if (randomSide == 0 && f == true)
         {
             polarBear.transform.position = new Vector3(-1, 0.23f, -0.23f);
             a = false; b = false; c = false; d = false; e = false; f = false; g = true;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         if (randomSide == 1 && f == true)
         {
             polarBear.transform.position = new Vector3(1, 0.23f, -0.23f);
             a = false; b = false; c = false; d = false; e = false; f = false; g = true;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         
         //e
@@ -80,73 +84,81 @@ public class Shooting : MonoBehaviour
         {
             polarBear.transform.position = new Vector3(-2, 0.23f, -0.23f);
             a = false; b = false; c = false; d = false; e = false; f = true;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         if (randomSide == 1 && e == true)
         {
             polarBear.transform.position = new Vector3(2, 0.23f, -0.23f);
             a = false; b = false; c = false; d = false; e = false; f = true;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         //d
         if (randomSide == 0 && d == true)
         {
             polarBear.transform.position = new Vector3(-3, 0.23f, -0.23f);
             a = false; b = false; c = false; d = false; e = true; f = false;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         if (randomSide == 1 && d == true)
         {
             polarBear.transform.position = new Vector3(3, 0.23f, -0.23f);
             a = false; b = false; c = false; d = false; e = true; f = false;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         //c
         if (randomSide == 0 && c == true)
         {
             polarBear.transform.position = new Vector3(-4, 0.23f, -0.23f);
             a = false; b = false; c = false; d = true; e = false; f = false;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         if (randomSide == 1 && c == true)
         {
             polarBear.transform.position = new Vector3(4, 0.23f, -0.23f);
             a = false; b = false; c = false; d = true; e = false; f = false;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         //b
         if (randomSide == 0 && b == true)
         {
             polarBear.transform.position = new Vector3(-5, 0.23f, -0.23f);
             a = false; b = false; c = true; d = false; e = false; f = false;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         if (randomSide == 1 && b == true)
         {
             polarBear.transform.position = new Vector3(5, 0.23f, -0.23f);
             a = false; b = false; c = true; d = false; e = false; f = false;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         //a
         if (randomSide == 0 && a == true)
         {
             polarBear.transform.position = new Vector3(-6, 0.23f, -0.23f);
             a = false; b = true; c = false; d = false; e = false; f = false;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         if (randomSide == 1 && a == true)
         {
             polarBear.transform.position = new Vector3(6, 0.23f, -0.23f);
             a = false; b = true; c = false; d = false; e = false; f = false;
-            Invoke("PolarBearWalk", 0.2f);
+            Invoke("PolarBearWalk", 0.1f);
         }
         //g
         
     }
+    
     void FixedUpdate()
     {
+        timer -= (Time.deltaTime);
+        timerText.text = "Time Left: " + ((int)timer);
+        if (timer<=0)
+        {
+            Time.timeScale = 0;
+        }
         if (Input.GetKey(KeyCode.Space))
         {
+            gunFireSound.SetActive(true);
             //Debug.Log("Hýz :" + sliderRandomValue);
            // Debug.Log("Hedef :" + sliderShootRandomValue);
             if (sliderShootRandomValue>=(shootingSlider.value-5) && sliderShootRandomValue<=(shootingSlider.value+5)) // hedef:60 value:65 value fazlasý ve eksisi: 55 ve 65
@@ -156,14 +168,14 @@ public class Shooting : MonoBehaviour
                     isShootedBear = true;
                     polarBearSpriteRenderer.color = Color.red;
                     Invoke("PolarBearShooted", 0.5f);
-                    
                 }
             }
+           
             if (isShooted==false)
             {
                 isShooted = true;
                 handGun.transform.position = new Vector3(1.142f, -0.3f, -0.17f);
-                Invoke("GunShooted", 0.2f);
+                Invoke("GunShooted", 0.3f);
             }
             shootingSlider.value = 0;
             sliderRandomValue = Random.Range(10, 50);
@@ -174,7 +186,7 @@ public class Shooting : MonoBehaviour
         {
             shootingSlider.value = 0;
         }
-        pointText.text = points.ToString();
+        pointText.text = "Points: "+points.ToString();
         shootAreaObj.transform.position= new Vector3((((sliderShootRandomValue*(0.0925f)) + 0.0497f)-4.97f), -4.83f, -0.86f);//value 0 -510 value 100 51
         //Debug.Log((sliderShootRandomValue * (5.61f) - 510));
     }
@@ -182,6 +194,7 @@ public class Shooting : MonoBehaviour
     {
         handGun.transform.position = new Vector3(1.142f, -0.5f, -0.17f);
         isShooted = false;
+        gunFireSound.SetActive(false);
     }
     void PolarBearShooted()
     {
